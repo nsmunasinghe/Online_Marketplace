f261a45a5e1fe340f031d79063ea40a278d36e90
Nimesh S. Munasinghe <nimeshsmunasinghe@gmail.com>
2024-02-24 17:36:07 +0530

    Initial commit

---
+++ home/urls.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import views as auth_view
+from django.urls import path
+
+from . import views
+from .forms import LoginForm
+
+app_name = 'home'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_view.LoginView.as_view(template_name='login.html', authentication_form=LoginForm), name='login'),
+]
